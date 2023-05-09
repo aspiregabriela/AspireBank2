dac50a873e270e5800723b76070fec38dcbaad80
Usuario <Usuario@CPD03-M15>
2023-05-09 15:16:29 -0300

    Adicionar arquivos de projeto.

---
+++ AspireBank/AspireBank/View/CriarConta.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AspireBank.View
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CriarConta : ContentPage
+	{
+		public CriarConta ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                App.Current.MainPage = new NavigationPage(new Login());
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("error", ex.Message, "OK");
+            }
+        }
+    }
+}
---
+++ AspireBank/AspireBank/View/Login.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AspireBank.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+
+
+            img_logo.Source = ImageSource.FromResource("AspireBank");
+
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            string usuario = txt_usuario.Text;
+            string senha = txt_senha.Text;
+
+            string usuario_certo = "gabs";
+            string senha_correta = "aspiregabs";
+            if (usuario == usuario_certo && senha == senha_correta)
+            {
+                App.Current.Properties.Add("usuario_logado", usuario);
+                // App.Current.MainPage = new NavigationPage(new Correntista());
+            }
+            else
+                DisplayAlert("Ops!", "Usuário ou senha incorretos.", "OK");
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+            try
+            {
+                App.Current.MainPage = new NavigationPage(new CriarConta());
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Error", ex.Message, "OK");
+            }
+        }
+    }
+}
