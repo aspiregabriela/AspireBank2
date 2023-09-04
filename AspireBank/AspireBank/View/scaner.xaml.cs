52cd5741e230cf59490dcba9a41f19193870ac55
Usuario <Usuario@CPD02-M15>
2023-09-04 09:40:19 -0300

    inserir codigo com ZXingScannerView

---
+++ AspireBank/AspireBank/View/scaner.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AspireBank.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class scaner : ContentPage
+    {
+        public scaner()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
